563d03af4390818ef685a3fc63861cf5e6e3617b
Dibyojyoti Deb <111603016+dibyojyoti-deb@users.noreply.github.com>
2026-01-14 07:27:14 +0000

    Codes

---
+++ File_Operations.py
@@ -0,0 +1,19 @@
+#Read from File
+file=open("f1.txt","r")
+content=file.readline()
+content1=file.readlines()
+print(content)
+print(content1)
+file.close()
+
+#Append to file
+file=open("f1.txt","a")
+file.write("\n New Line Added")
+
+file.close()
+
+#Write to File
+file=open("f1.txt","w")
+file.write("Hello Python\n")
+file.write("This is my write example")
+file.close()

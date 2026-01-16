d8be97ea0bcca6228ba31638214d0b22a45e2b54
Dibyojyoti Deb <111603016+dibyojyoti-deb@users.noreply.github.com>
2026-01-16 12:54:59 +0000

    Codes

---
+++ Assignment Questions/Day 4 Assignment/Multiple_Abstraction.py
@@ -0,0 +1,11 @@
+import subprocess
+result=subprocess.run(
+    "echo Hello World",
+    shell=True,
+    capture_output=True,
+    text=True
+)
+print(result.stdout)
+
+
+subprocess.run(("python","Multiple_Abstraction.py"))
